919008c302dd08a6735111421a5fd0d101a4cf21
Michael Westblade <michaelwestblade@gmail.com>
2015-01-18 23:11:07 -0600

    removed default copyright info

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <div class="footer">
     <nav class="navbar navbar-default HeadersBGColor" role="navigation">
         <div class="container">
-            <?php echo get_theme_mod( 'copyright_textbox', '' ); ?>
+            <?php echo get_theme_mod( 'copyright_textbox', '&copy;' ); ?>
             <?php echo ( get_theme_mod( 'show_copyright_year' ) != '' ? "&copy; ".date('Y') : ''  ); ?>
         </div>
     </nav>
