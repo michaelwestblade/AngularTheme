b73ce5a9c0e1c11fb242ef07d07b957163d5380e
Michael Westblade <michaelwestblade@gmail.com>
2015-01-21 23:07:19 -0600

    instagram should work without login

--- libs/Instagram.php
+++ libs/Instagram.php
@@ -24,5 +24,25 @@ function getInstagramPhotos(){
     die();
 }
 
+function fetchInstagramPhotos(){
+    $options = get_option('plugin_options');
+    $url = "https://api.instagram.com/v1/users/{$options['instagram_account_id']}/media/recent/?client_id={$options['instagram_client_id']}";
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)");
+    $feed = json_decode(curl_exec($ch));
+    curl_close($ch);
+
+    $result = json_encode($feed);
+    $fp = fopen(get_template_directory().'/json/instagram.json', 'w')
+    or die("Error opening output file");
+    fwrite($fp, $result);
+    fclose($fp);
+}
+
 add_action( 'wp_ajax_nopriv_getInstagramPhotos', 'getInstagramPhotos' );
 add_action( 'wp_ajax_getInstagramPhotos', 'getInstagramPhotos' );
