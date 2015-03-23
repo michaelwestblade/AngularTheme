347b9535b46a08b2aebcadb6f1c5853731df3a4f
Michael Westblade <michaelwestblade@gmail.com>
2015-03-22 20:42:18 -0500

    fixed my unnerving stupidity

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <html <?php language_attributes(); ?> ng-app="myapp">
     <head>
         <title><?php echo get_bloginfo('name'); ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
         <base href=""/>
         <?php
